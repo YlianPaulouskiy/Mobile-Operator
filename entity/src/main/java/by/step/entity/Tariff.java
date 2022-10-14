aea6bd226d18b3008ca1f7dc343ee88b8c3a94e6
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-10-14 09:46:21 +0300

    Entity module: add Tariff entity

---
+++ entity/src/main/java/by/step/entity/Tariff.java
@@ -0,0 +1,22 @@
+package by.step.entity;
+
+import by.step.entity.parent.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+public class Tariff extends BaseEntity {
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    private Integer minutes;
+    
+    private Integer megabytes;
+
+}
