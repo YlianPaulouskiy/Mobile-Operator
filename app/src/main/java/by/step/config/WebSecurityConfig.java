a9b84fa28fee5b17380de019fc1ac94c99a7ede4
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-10-27 21:14:38 +0300

    Some changes in securityConfiguration

--- app/src/main/java/by/step/config/WebSecurityConfig.java
+++ app/src/main/java/by/step/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and().logout();
